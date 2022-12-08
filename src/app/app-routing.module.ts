5ea8a2407599769f383337e9cd97bb0580173740
tahermarzouk <tahermarzouk0@gmail.com>
2022-12-08 12:34:17 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllCandidaturesComponent } from './all-candidatures/all-candidatures.component';
+import { CandidatureComponent } from './candidature/candidature.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "candidatureForm",
+    component: CandidatureComponent,
+  },  {
+    path: "allCandidatures",
+    component: AllCandidaturesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,47 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CandidatureComponent } from './candidature/candidature.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips'; 
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule} from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
+import { AllCandidaturesComponent } from './all-candidatures/all-candidatures.component';
+import { MatTableModule } from '@angular/material/table';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidatureComponent,
+    AllCandidaturesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+    MatTableModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
